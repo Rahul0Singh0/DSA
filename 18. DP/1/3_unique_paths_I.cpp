0fb9c0ad6d03bf400a80ae938627aa7564af38a8
Rahul Singh <thesincererahul@gmail.com>
2024-08-19 12:50:00 +0530

    update unique paths

--- 18. DP/1/3_unique_paths_I.cpp
+++ 18. DP/1/3_unique_paths_I.cpp
@@ -25,4 +25,10 @@ public:
         vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
         return fun(m,n,dp);
     }
+    // Precise code
+    int uniquePaths(int m, int n) {
+        if(m == 1 and n == 1) return 1;
+        if(m == 0 or n == 0) return 0;
+        return uniquePaths(m-1, n)+uniquePaths(m, n-1);
+    }
 };
