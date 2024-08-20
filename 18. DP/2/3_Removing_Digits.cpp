98f21b55b241611453080f2ecda1ea106ba8d24f
Rahul Singh <thesincererahul@gmail.com>
2024-08-20 15:20:36 +0530

    Removing Digits solved by bottom to up Tabulation method

--- 18. DP/2/3_Removing_Digits.cpp
+++ 18. DP/2/3_Removing_Digits.cpp
@@ -25,11 +25,26 @@ int f(int n) {
     }
     return dp[n] = 1 + result;
 }
+int fbu(int num) {
+    dp[0] = 0;
+    for(int i = 1; i <= 9; i++) dp[i] = 1;
+    for(int n = 10; n <= num; n++) {
+        // n -> state
+        // calc dp[n]
+        vector<int> d = get_digits(n);
+        int result = INT_MAX;
+        for(int i = 0; i < d.size(); i++) {
+            result = min(result, dp[n-d[i]]);
+        }
+        dp[n] = 1 + result;
+    }
+    return dp[num];
+}
 int main() {
     int n;
     cin>>n;
     dp.clear();
     dp.resize(1000005,-1);
-    cout<<f(n);
+    cout<<fbu(n);
     return 0;
 }
