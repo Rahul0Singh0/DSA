8d4d88b1ebc9f9a2d5850df3ac738df7f8a399f9
Rahul Singh <thesincererahul@gmail.com>
2024-10-02 09:01:15 +0530

    minimum knight moves

---
+++ 19. Graph/3/5_Minimum_Knight_Moves.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/minimum-knight-moves/
+
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<set>
+using namespace std;
+
+struct Solution {
+    int minKnightMoves(int x, int y) {
+        queue<pair<int, int>> qu;
+        set<pair<int, int>> vis;
+        vector<vector<int>> dir = {{-1, 2}, {-1, -2}, {-2, -1}, {-2, 1}, {1, 2}, {1, -2}, {2, -1}, {2, 1}};
+        vis.insert({0, 0});
+        qu.push({0, 0});
+        int cnt = 0;
+        while(not qu.empty()) {
+            for(int k = qu.size(); k > 0; --k) {
+                auto [i, j] = qu.front();
+                qu.pop();
+                if(i == x && j == y) return cnt;
+                for(auto d : dir) {
+                    int ni = d[0] + i;
+                    int nj = d[1] + j;
+                    if(vis.find({ni, nj}) == vis.end()) {
+                        vis.insert({ni, nj});
+                        qu.push({ni, nj});
+                    }
+                }
+            }
+            cnt++;
+        }
+        return -1;
+    }
+};
+
+int main() {
+    Solution s;
+    int x, y;
+    cin>>x>>y;
+    cout<<s.minKnightMoves(x, y);
+    return 0;
+}
