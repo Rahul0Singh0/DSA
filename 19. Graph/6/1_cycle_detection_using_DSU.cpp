f6c05165bf96f0013db7c5cdd2d13a9d7b8b357a
Rahul Singh <thesincererahul@gmail.com>
2024-10-13 15:05:16 +0530

    Cycle Detection in undirected graph using DSU

---
+++ 19. Graph/6/1_cycle_detection_using_DSU.cpp
@@ -0,0 +1,42 @@
+// In Undirected Graph, detect a cycle
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int find(vector<int> &parent, int x) {
+    return parent[x] = (parent[x] == x) ? x : find(parent, parent[x]);
+}
+// Time: E x log*(v) => log*(v) = constant
+// Time: O(E)
+bool Union(vector<int> &parent, vector<int> &rank, int a, int b) {
+    a = find(parent, a);
+    b = find(parent, b);
+    if(a == b) return true; // parent same of a and b
+    if(rank[a] >= rank[b]) {
+        rank[a]++;
+        parent[b] = a;
+    } else {
+        rank[b]++;
+        parent[a] = b;
+    }
+    return false;
+}
+
+int main() {
+    int n, m;
+    cin>>n>>m;
+    // n is no. of elements, and m is no. of queries
+    vector<int> parent(n+1);
+    vector<int> rank(n+1, 0);
+    for(int i = 0; i <= n; i++) {
+        parent[i] = i;
+    }
+    while(m--) {
+        int x, y;
+        cin>>x>>y;
+        bool b = Union(parent, rank, x, y);
+        if(b == true) cout<<"Cycle detected\n";
+    }
+    cout<<"Cycle not detected\n";
+    return 0;
+}
