a3ee1b6636149583e625248342b0645b9560ba19
Rahul Singh <thesincererahul@gmail.com>
2024-10-14 10:22:01 +0530

    Add function of Union by size

--- 19. Graph/4_DSU/1_DSU.cpp
+++ 19. Graph/4_DSU/1_DSU.cpp
@@ -10,19 +10,35 @@ int find(vector<int> &parent, int x) {
 }
 
 // T.C:  O(log*n)
-void Union(vector<int> &parent, vector<int> &rank, int a, int b) {
+// Union by Rank(Improved version)
+// void Union(vector<int> &parent, vector<int> &rank, int a, int b) {
+//     a = find(parent, a);
+//     b = find(parent, b);
+//     if(a == b) return; // both a and b are same set
+//     // Union by rank
+//     if(rank[a] >= rank[b]) {
+//         rank[a]++;
+//         parent[b] = a;
+//     } else {
+//         rank[b]++;
+//         parent[a] = b;
+//     }
+// }
+
+// Union by size
+void Union(vector<int> &parent, vector<int> &Size, int a, int b) {
     a = find(parent, a);
     b = find(parent, b);
-    if(a == b) return; // both a and b are same set
-    // Union by rank
-    if(rank[a] >= rank[b]) {
-        rank[a]++;
+    if(a == b) return; // parent same of a and b
+    if(Size[a] >= Size[b]) {
+        Size[a] += Size[b];
         parent[b] = a;
     } else {
-        rank[b]++;
+        Size[b] += Size[a];
         parent[a] = b;
     }
 }
+
 
 int main() {
     int n, m;
@@ -30,8 +46,12 @@ int main() {
     // n is no. of elements, and m is no. of queries
     vector<int> parent(n+1);
     vector<int> rank(n+1, 0);
+    vector<int> Size(n+1);
+
+    // initially each and every element has parent itself
     for(int i = 0; i <= n; i++) {
         parent[i] = i;
+        Size[i] = i;
     }
     while(m--) {
         string str;
@@ -39,7 +59,8 @@ int main() {
         if(str == "union") {
             int x, y;
             cin>>x>>y;
-            Union(parent, rank, x, y);
+            // Union(parent, rank, x, y);
+            Union(parent, Size, x, y);
         } else {
             int x;
             cin>>x;
