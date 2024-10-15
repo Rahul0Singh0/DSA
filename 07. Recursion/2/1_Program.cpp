a076801a55663189bb5d562a041cd5de22151067
Rahul Singh <thesincererahul@gmail.com>
2024-10-15 19:04:29 +0530

    sum of nth fibonacci number

---
+++ 07. Recursion/2/1_Program.cpp
@@ -0,0 +1,15 @@
+// Write a function to calculate the nth fibonacci number using recursion
+#include <iostream>
+using namespace std;
+
+int f(int n) {
+    if(n == 1 || n == 2) return 1;
+    return f(n-1) + f(n-2);
+}
+
+int main() {
+    int n;
+    cin>>n;
+    cout<<f(n)<<"\n";
+    return 0;
+}
