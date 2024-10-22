275660e7083b4b2d03f8c8d34506f9feb0dccf3b
Rahul Singh <thesincererahul@gmail.com>
2024-10-22 10:03:24 +0530

    Maze path

--- 07. Recursion/2/4_Maze_Path.cpp
+++ 07. Recursion/2/4_Maze_Path.cpp
@@ -8,14 +8,17 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
+int dp[1005][1005];
 int f(int i, int j, int n, int m) {
     if(i < 0 or j < 0 or i > n or j > m) return 0;
     if(i == n and j == m) return 1;
-    return f(i+1, j, n, m) + f(i, j+1, n, m);
+    if(dp[i][j] != -1) return dp[i][j];
+    return dp[i][j] = f(i+1, j, n, m) + f(i, j+1, n, m);
 }
 int main() {
     int n,m;
     cin>>n>>m;
+    memset(dp, -1, sizeof dp);
     cout<<f(0,0,n-1,m-1);
     return 0;
 }
