513d756606638311ce6525ce5090169865e58b23
Rahul Singh <thesincererahul@gmail.com>
2025-02-14 06:50:30 +0530

    lower bound

--- 06. Searching/1/3_LowerBound.cpp
+++ 06. Searching/1/3_LowerBound.cpp
@@ -1,6 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int LowerBound(int arr[], int n, int x) {
+
+int BruteForce(int arr[], int n, int x) {
+    for(int i = 0; i < n; i++) {
+        if(arr[i] >= x) return i;
+    }
+    return n;
+}
+
+int lowerbound(int arr[], int n, int x) {
+    int lo = 0, hi = n-1;
+    int ans = n;
+    
+    while(lo <= hi) {
+        int mid = lo + (hi - lo) / 2;
+        if(arr[mid] >= x) {
+            ans = mid;
+            // look for smaller idx on the left
+            hi = mid - 1;
+        } else  {
+            lo = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int LowerBound1(int arr[], int n, int x) {
     int l = 0, r = n - 1;
     while (l < r) {
         int mid = l + (r - l) / 2;
@@ -13,10 +38,20 @@ int LowerBound(int arr[], int n, int x) {
     return l;
 }
 
+int LowerBound(int arr[], int n, int x) {
+    int l = 0, r = n-1;
+    while(l <= r) {
+        int mid = l + (r - l) / 2;
+        if(arr[mid] == x) return mid - 1;
+        else if(arr[mid] < x) l = mid + 1;
+        else r = mid - 1;
+    }
+}
+
 int main() {
     int arr[] = {1, 2, 3, 3, 3, 4, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
-    cout << LowerBound(arr, n, 3) << endl;
-    cout<<lower_bound(arr,arr+n,3)-arr<<endl;
+    cout << arr[LowerBound(arr, n, 3)] << endl;
+    cout<<*lower_bound(arr,arr+n,3)<<endl;
     return 0;
 }
