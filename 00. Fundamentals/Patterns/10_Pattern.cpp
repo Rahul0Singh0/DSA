e491537a4e034d82c57c63fc1ffbc8cd4bb822c4
Rahul Singh <thesincererahul@gmail.com>
2025-03-13 17:14:23 +0530

    added pattern problems

--- 00. Fundamentals/Patterns/10_Pattern.cpp
+++ 00. Fundamentals/Patterns/10_Pattern.cpp
@@ -12,7 +12,7 @@ void pattern(int n) {
         for(int j = 1; j <= n-i; j++) {
             std::cout<<" ";
         }
-        for(int j = 1; j <= n; j++) {
+        for(int j = 1;  j <= n; j++) {
             std::cout<<"*";
         }
         std::cout<<std::endl;
@@ -40,9 +40,60 @@ void pattern1(int n) {
     }
 }
 
+/*
+   1
+  121
+ 12321
+1234321
+*/
+
+void pattern2(int n) {
+    for(int i = 1; i <= n; i++) {
+        int j;
+        for(j = 1; j <= n-i; j++) {
+            std::cout<<" ";
+        }
+        for(j = 1; j <= i; j++) {
+            std::cout<<j;
+        }
+        for(int k = j-2; k >= 1; k--) {
+            std::cout<<k;
+        }
+        std::cout<<std::endl;
+    }
+}
+
+/*
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+*/
+
+void pattern3(int n) {
+    int a = 1;
+    int b = n-1;
+    for(int i = 1; i <= n*2-1; i++) {
+        for(int j = 1; j <= b; j++) {
+            std::cout<<" ";
+        }
+        if(i <= n-1) b--;
+        else b++;
+        for(int j = 1; j <= a; j++) {
+            std::cout<<"*";
+        }
+        if(i <= n-1) a += 2;
+        else a -= 2;
+        std::cout<<std::endl;
+    }
+}
+
 int main() {
     int n;
     std::cin>>n;
-    pattern1(n);
+    pattern3(n);
     return 0;
 }
