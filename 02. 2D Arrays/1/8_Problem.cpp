4dde0e9764e8b8dcf1837f1292ec4ddca389f1bc
Rahul Singh <thesincererahul@gmail.com>
2025-03-25 12:09:12 +0530

    Transpose without extra space

---
+++ 02. 2D Arrays/1/8_Problem.cpp
@@ -0,0 +1,33 @@
+// You are given a matrix of size NxN.
+// Change this matrix into its transpose without using extra space
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<vector<int>> Vector;
+
+void fun(Vector& arr) {
+    int n = arr.size();
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i == j) break;
+            swap(arr[i][j], arr[j][i]);
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin>>n;
+    Vector v(n, vector<int>(n));
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin>>v[i][j];
+        }
+    }
+    fun(v);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
