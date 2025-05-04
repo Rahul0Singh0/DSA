6975e4475016aee7664efa199a5b32fde3264336
Rahul Singh <thesincererahul@gmail.com>
2025-05-04 13:11:56 +0530

    sliding window technique

--- 09. Special Algorithms/SlidingWindow/Problems/Leetcode2962.cpp
+++ 09. Special Algorithms/SlidingWindow/Problems/Leetcode2962.cpp
@@ -21,4 +21,22 @@ public:
         }
         return res;
     }
+    // Sliding Window O(N)
+    long long fun(vector<int> &nums, int k) {
+        const int mx_ele = ranges::max(nums);
+        long long int res = 0;
+        int cnt = 0;
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == mx_ele) cnt++;
+
+            while (cnt == k) {
+                if (nums[j] == mx_ele) cnt--;
+                j++; // count number of subarrays with atleast k elements
+            }
+
+            res += j;
+        }
+        return res;
+    }
 };
